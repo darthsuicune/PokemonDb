6c806eb6764bf2607cc51574cdfb462437f38fab
Denis Lapuente <darthsuicune@gmail.com>
2016-09-09 20:27:16 +0200

    Update dependency

---
+++ Database/test/com/dlgdev/pokemon/database/PokemonDatabaseTest.java
@@ -0,0 +1,19 @@
+package com.dlgdev.pokemon.database;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class PokemonDatabaseTest {
+	PokemonDatabase db;
+	@Before public void setUp() throws Exception {
+		db = new PokemonDatabase(null);
+	}
+
+	@Test public void testFind() throws Exception {
+		Pokemon mon = db.find(0,0);
+		assertNotNull(mon);
+	}
+
+}
