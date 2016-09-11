dac87c3d95f2a79a742b83f4d4317def73832643
Denis Lapuente <darthsuicune@gmail.com>
2016-09-12 00:24:00 +0200

    Add name

--- Data/src/com/dlgdev/pokemon/database/Pokemon.java
+++ Data/src/com/dlgdev/pokemon/database/Pokemon.java
@@ -3,6 +3,7 @@ package com.dlgdev.pokemon.database;
 public class Pokemon {
 	final int dexNumber;
 	final int formNumber;
+	public String name;
 
 	public Pokemon(int dexNumber, int formNumber) {
 		this.dexNumber = dexNumber;
